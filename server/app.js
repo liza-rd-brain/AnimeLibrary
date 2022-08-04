cb5075d13caaf1b8fc3bbbc42629bdfed0ed87b3
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2022-08-04 18:51:18 +0300

    small fix

--- server/app.js
+++ server/app.js
@@ -11,64 +11,51 @@ const menuNavSelector = "#menubtn";
 
 const searchFormSelector = "#m_search";
 const searchInputSelector = "#story";
-// const searchButtonSelector = `${searchInputSelector} + button[type='submit']`;
 const searchButtonSelector = ".m_search_btn";
 const screenshotPath_1 = "anime_1.png";
 const screenshotPath = "anime_2.png";
 
 const articleSelector = "article.shortstory";
 
+async function makeScraping(browser) {
+  const page = await browser.newPage();
+
+  await page.goto(url);
+  await page.click(menuNavSelector);
+
+  await page.focus(searchInputSelector);
+  await page.type(searchInputSelector, animeName);
+  await page.keyboard.press("Enter");
+  await page.waitForNavigation();
+  // await page.screenshot({ path: `${Math.random()}.png` });
+
+  // const titleSelector = ".titleup";
+  const animeList = await page.$$eval(articleSelector, (list) => {
+    const newList = list.reduce((prev, item, index) => {
+      //Почему const titleSelector  не определена
+      const titleElem = item.querySelector(".titleup");
+      const titleText = titleElem.innerText;
+
+      return [...prev, titleText];
+    }, []);
+
+    return newList;
+  });
+
+  console.log(animeList);
+
+  await page.screenshot({ path: screenshotPath });
+}
+
 app.get("/", (req, res) => {
   puppeteer.launch().then(async function (browser) {
-    const page = await browser.newPage();
-    //ждем загрузки страницы
-    await page.goto(url);
-
-    // await page.screenshot({ path: screenshotPath });
-
-    console.log("before type");
-    //1. Открываем панель меню
-    await page.screenshot({ path: screenshotPath_1 });
-    await page.click(menuNavSelector);
-    await page.focus(searchInputSelector);
-    await page.type(searchInputSelector, animeName);
-    await page.keyboard.press("Enter");
-    //Ждем загрузку страницы
-    await page.waitForNavigation();
-    await page.screenshot({ path: screenshotPath });
-
-    // const digimonNames = await page.$$eval(
-    //   "#digiList tbody tr td:nth-child(2) a",
-    //   function (digimons) {
-    //     // Mapping each Digimon name to an array
-    //     return digimons.map(function (digimon) {
-    //       return digimon.innerText;
-    //     });
-    //   }
-    // );
-    const titleSelector = ".titleup";
-
-    const animeList = await page.$$eval(articleSelector, (list) => {
-      const newList = list.reduce((prev, item, index) => {
-        console.log(item);
-        const titleElem = item.querySelector(".titleup");
-        const titleText = titleElem.innerText;
-
-        console.log(titleElem);
-        return [...prev, titleText];
-      }, []);
-
-      return newList;
-    });
-
-    // А что отдать, если элемент  не найден, добавить обработку ошибок !!
-
-    console.log("success type");
-
-    console.log(animeList);
-    await browser.close();
-
-    //затем ищем строку поиска
+    try {
+      await makeScraping(browser);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      await browser.close();
+    }
   });
 });
 
