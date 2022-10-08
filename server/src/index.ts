988a8a7d749daee683adac52a85370757551e021
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2022-10-09 01:23:39 +0300

    took out makeScraping

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,32 +1,16 @@
 const cors = require("cors");
 const express = require("express");
-const puppeteer = require("puppeteer");
 const bodyParser = require("body-parser");
 
 import * as http from "http";
 import * as WebSocket from "ws";
-
-import { store } from "./data";
-import { DetailAnimeList, RawDetailAnime } from "./types";
-import { takeLinkList } from "./business/takeLinkList";
-import { Request, Response } from "express";
-import { getAnimeDetail } from "./business/getAnimeDetail";
-import { getStructuredDetail } from "./business/getStructuredDetail";
-import { getDetailLinkList } from "./business/getDetailLinkList";
-
-const webSocketConnection = { connect: false };
+import { makeScraping } from "./business/makeScraping";
 
 const app = express();
 const PORT = 3000;
-/* __________________________websocket______________ */
 
-//initialize a simple http server
 const server = http.createServer(app);
-
-//initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
-
-const state = { isAlive: true };
 
 wss.on("connection", (ws: WebSocket) => {
   const controller = new AbortController();
@@ -41,11 +25,11 @@ wss.on("connection", (ws: WebSocket) => {
 
     makeScraping(animeName, controller)
       .then(
-        (detailAnimeList) => {
+        ([detailAnimeList]) => {
           const detailAnimeJSON = JSON.stringify(detailAnimeList);
           ws.send(detailAnimeJSON);
         },
-        (err) => {
+        ([err]) => {
           console.log(" scrape failed", err);
         }
       )
@@ -58,59 +42,6 @@ server.listen(process.env.PORT || PORT, () => {
   console.log(`Server started on port ${address.port} :)`);
 });
 
-/* __________________________websocket______________ */
-
-const chromeOptions = {
-  headless: false,
-  defaultViewport: null,
-  slowMo: 100,
-};
-
 app.use(cors());
+
 app.use(bodyParser.json());
-
-const makeScraping = async (
-  animeName: string,
-  controller: AbortController
-): Promise<DetailAnimeList> => {
-  const browser = await puppeteer.launch(chromeOptions);
-  console.log("browser", browser);
-
-  return new Promise<DetailAnimeList>(async (resolve, reject) => {
-    try {
-      controller.signal.addEventListener("abort", () => {
-        console.log("прерывать scraping");
-        reject("err close scraping");
-      });
-
-      const page = await browser.newPage();
-
-      let detailList: Array<RawDetailAnime> = [];
-      const initialList = await takeLinkList(page, animeName);
-
-      const listWithDetails = getDetailLinkList(initialList);
-
-      for (let i = 0; i < listWithDetails.length; i++) {
-        const detailItem: RawDetailAnime = await getAnimeDetail(
-          listWithDetails[i],
-          page
-        );
-        detailList.push(detailItem);
-      }
-
-      const structuredDetailList: DetailAnimeList =
-        getStructuredDetail(detailList);
-
-      resolve(structuredDetailList);
-    } catch {
-      reject();
-    }
-  })
-    .catch((err) => {
-      console.log("makeScraping err", err);
-      throw err;
-    })
-    .finally(() => {
-      return browser.close();
-    });
-};
