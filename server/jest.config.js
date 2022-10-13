37bb5001e405fe85697f658d0fb187a9aa99f3b4
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2022-10-13 17:20:57 +0300

    build seem to work

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  modulePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/build/"],
-  // transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  modulePathIgnorePatterns: ["<rootDir>/build/"],
 };
