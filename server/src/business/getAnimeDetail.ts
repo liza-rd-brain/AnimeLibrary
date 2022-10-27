d34fed709ba0887579677de705e721d5eb3505f0
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2022-10-27 04:49:41 +0300

    some type fix

--- server/src/business/getAnimeDetail.ts
+++ server/src/business/getAnimeDetail.ts
@@ -1,6 +1,7 @@
+import * as puppeteer from "puppeteer";
 import { RawDetailAnime } from "../types";
 
-export async function getAnimeDetail(item, page) {
+export async function getAnimeDetail(item: string, page: puppeteer.Page) {
   const descBlockItems = ".dp-i-content";
 
   await page.goto(item);
@@ -9,19 +10,28 @@ export async function getAnimeDetail(item, page) {
   try {
     const animeDetailItem: RawDetailAnime = await page.$eval(
       descBlockItems,
-      (item) => {
+      (item: Element) => {
         const descriptionSelector = ".description p";
         const pictureSelector = ".film-poster-img";
         const detailSelector = ".elements .row-line";
         const animeNameSelector = ".heading-name";
 
-        const pictureUrl: string = item.querySelector(pictureSelector).src;
-        const animeName = item.querySelector(animeNameSelector).innerText;
+        const pictureElem: HTMLImageElement | null =
+          item.querySelector(pictureSelector);
+        const pictureUrl = pictureElem ? pictureElem.src : undefined;
 
-        const description: string =
-          item.querySelector(descriptionSelector).innerText;
+        const animeNameElem: HTMLElement | null =
+          item.querySelector(animeNameSelector);
+        const animeName = animeNameElem ? animeNameElem.innerText : undefined;
 
-        const enitityNodeList = item.querySelectorAll(detailSelector);
+        const descriptionElem: HTMLElement | null =
+          item.querySelector(descriptionSelector);
+        const description = descriptionElem
+          ? descriptionElem.innerText
+          : undefined;
+
+        const enitityNodeList: NodeListOf<HTMLElement> =
+          item.querySelectorAll(detailSelector);
         const entityItemList: Array<HTMLElement> = Array.from(enitityNodeList);
 
         const detailTextList = entityItemList.map((item) => item.innerText);
