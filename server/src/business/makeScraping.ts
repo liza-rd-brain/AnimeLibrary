f3ab28790cdb8cc4860d3d35fabf960d19972916
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2022-11-14 22:19:29 +0300

    prepare for deploy

--- server/src/business/makeScraping.ts
+++ server/src/business/makeScraping.ts
@@ -1,4 +1,4 @@
-import * as puppeteer from "puppeteer";
+// import * as puppeteer from "puppeteer";
 
 import { AnimeHashTable } from "types";
 import { RawDetailAnime } from "../types";
@@ -6,50 +6,70 @@ import { takeLinkList } from "./takeLinkList";
 import { getAnimeDetail } from "./getAnimeDetail";
 import { getDetailLinkList } from "./getDetailLinkList";
 import { getStructuredDetailHash } from "./getStructuredDetailHash";
+import { Browser, PuppeteerLaunchOptions } from "puppeteer";
 
-const chromeOptions = {
-  headless: true,
-  defaultViewport: null,
-  slowMo: 100,
-};
+//TODO: временная мера, исправить тип
+let chrome: any = {};
+let puppeteer;
 
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
 export const makeScraping = async (
   animeName: string,
   controller: AbortController
-): Promise<[AnimeHashTable, puppeteer.Browser]> => {
+): Promise<[AnimeHashTable, Browser]> => {
+  let chromeOptions: PuppeteerLaunchOptions = {
+    headless: false,
+    defaultViewport: null,
+    slowMo: 100,
+  };
+
   const browser = await puppeteer.launch(chromeOptions);
+  // const browser = await puppeteer.launch(chrome);
 
-  return new Promise<[AnimeHashTable, puppeteer.Browser]>(
-    async (resolve, reject) => {
-      try {
-        controller.signal.addEventListener("abort", () => {
-          console.log("прерывать scraping");
-          reject(["err close scraping", browser]);
-        });
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    chromeOptions = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
 
-        const page = await browser.newPage();
-        let detailList: Array<RawDetailAnime> = [];
-        const initialList = await takeLinkList(page, animeName);
+  return new Promise<[AnimeHashTable, Browser]>(async (resolve, reject) => {
+    try {
+      controller.signal.addEventListener("abort", () => {
+        console.log("прерывать scraping");
+        reject(["err close scraping", browser]);
+      });
 
-        const linkList: Array<string> = getDetailLinkList(initialList);
+      const page = await browser.newPage();
+      let detailList: Array<RawDetailAnime> = [];
+      const initialList = await takeLinkList(page, animeName);
 
-        for (let i = 0; i < linkList.length; i++) {
-          const detailItem: Omit<RawDetailAnime, "link"> | null =
-            await getAnimeDetail(linkList[i], page);
+      const linkList: Array<string> = getDetailLinkList(initialList);
 
-          if (detailItem) {
-            const detailItemWithLink = { ...detailItem, link: linkList[i] };
-            detailList.push(detailItemWithLink);
-          }
+      for (let i = 0; i < linkList.length; i++) {
+        const detailItem: Omit<RawDetailAnime, "link"> | null =
+          await getAnimeDetail(linkList[i], page);
+
+        if (detailItem) {
+          const detailItemWithLink = { ...detailItem, link: linkList[i] };
+          detailList.push(detailItemWithLink);
         }
-
-        const structuredDetailList: AnimeHashTable =
-          getStructuredDetailHash(detailList);
-
-        resolve([structuredDetailList, browser]);
-      } catch (err) {
-        reject([err, browser]);
       }
+
+      const structuredDetailList: AnimeHashTable =
+        getStructuredDetailHash(detailList);
+
+      resolve([structuredDetailList, browser]);
+    } catch (err) {
+      reject([err, browser]);
     }
-  ).finally(() => browser.close());
+  }).finally(() => browser.close());
 };
